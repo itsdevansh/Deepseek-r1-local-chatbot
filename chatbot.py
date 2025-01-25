b293d1f814e240df6df29b0bdbd673defcf5f5ee
Rohit Shelke <rshel059@uottawa.ca>
2025-01-24 19:16:53 -0500

    Implemented event creation through llm

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,86 @@
 import os
-from langchain_ollama.llms import OllamaLLM
+from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
+from langchain_core.prompts import ChatPromptTemplate
 import io
+import json
+
+
+from create_event import create_event
 
 load_dotenv()
 
-MODEL_NAME = os.getenv("MODEL_NAME")
 
-def init_model(MODEL_NAME: str) -> OllamaLLM: 
+def init_model() -> ChatOllama:
     try:
-        model = OllamaLLM(model=MODEL_NAME)
-        print(model)
+        MODEL_NAME = os.getenv("MODEL_NAME")
+        llm = ChatOllama(
+            model=MODEL_NAME,
+            temperature=0.3,
+            # other params...
+        )
+        llm.format = "json"
+        print(llm)
+        return llm
     except Exception as e:
         print(f"Model cannot be initialized: {e}")
+
+
+def llm_create_event(llm: ChatOllama, user_input: str) -> str:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """You are an agent that creates google calendar events. Your output should be in the following format:{{
+      "summary": " ",
+      "location": " ",
+      "description": " ",
+      "start": {{
+        "dateTime": " ",
+        "timeZone": "America/Los_Angeles",
+      }},
+      "end": {{
+        "dateTime": " ",
+        "timeZone": "America/Los_Angeles",
+      }},
+      "recurrence": [
+        "RRULE:FREQ=DAILY;COUNT=1"
+      ],
+      "attendees": [
+        {{"email": " "}},
+      ],
+      "reminders": {{
+        "useDefault": False,
+        "overrides": [
+          {{"method": "email", "minutes": 24 * 60}},
+          {{"method": "popup", "minutes": 10}},
+        ],
+      }},
+    }}
+    From the user input, see if you can extract relevant information and for information that is missing, you can ask the user questions that you think are necessary to create the event. Remember to cause least friction to the user. Only ask information with technical details, any details that can be inferred from the user input, should be inferred geneously.
+    """,
+            ),
+            ("human", "{input}"),
+        ]
+    )
+
+    chain = prompt | llm
+
+    a = chain.invoke(
+        {
+            "input": user_input
+        }
+    )
+    response = a.content
+    return response
+
+
+if __name__ == "__main__":
+    llm = init_model()
+    user_input = "Can you create an event on 5th February 2025 11:00 AM - 2:00 PM to attend a conference at Unviersity of Ottawa with no attendees?"
+    data = llm_create_event(llm, user_input)
+    print(json.loads(data))
+    link = create_event(json.loads(data))
+    print(link)
--- create_event.py
+++ create_event.py
@@ -9,74 +9,71 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-def auth():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-    return creds
+
+"""Shows basic usage of the Google Calendar API.
+Prints the start and name of the next 10 events on the user's calendar.
+"""
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists("token.json"):
+  creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+  if creds and creds.expired and creds.refresh_token:
+    creds.refresh(Request())
+  else:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        "credentials.json", SCOPES
+    )
+    creds = flow.run_local_server(port=0)
+  # Save the credentials for the next run
+  with open("token.json", "w") as token:
+    token.write(creds.to_json())
 
   
-def create_event(creds, data: dict):
+def create_event(data: dict):
   try:
     service = build("calendar", "v3", credentials=creds)
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Crating event")
+    print("Creating event")
 
-    event = {
-      'summary': 'Google I/O 2015',
-      'location': '800 Howard St., San Francisco, CA 94103',
-      'description': 'A chance to hear more about Google\'s developer products.',
-      'start': {
-        'dateTime': f'{now}',
-        'timeZone': 'America/Los_Angeles',
-      },
-      'end': {
-        'dateTime': f'{now}',
-        'timeZone': 'America/Los_Angeles',
-      },
-      'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-      ],
-      'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-      ],
-      'reminders': {
-        'useDefault': False,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      },
-    }
+    # event = {
+    #   'summary': 'Google I/O 2015',
+    #   'location': '800 Howard St., San Francisco, CA 94103',
+    #   'description': 'A chance to hear more about Google\'s developer products.',
+    #   'start': {
+    #     'dateTime': f'{now}',
+    #     'timeZone': 'America/Los_Angeles',
+    #   },
+    #   'end': {
+    #     'dateTime': f'{now}',
+    #     'timeZone': 'America/Los_Angeles',
+    #   },
+    #   'recurrence': [
+    #     'RRULE:FREQ=DAILY;COUNT=2'
+    #   ],
+    #   'attendees': [
+    #     {'email': 'lpage@example.com'},
+    #     {'email': 'sbrin@example.com'},
+    #   ],
+    #   'reminders': {
+    #     'useDefault': False,
+    #     'overrides': [
+    #       {'method': 'email', 'minutes': 24 * 60},
+    #       {'method': 'popup', 'minutes': 10},
+    #     ],
+    #   },
+    # }
+
+    event = data
 
     event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
+    # print('Event created: %s' % (event.get('htmlLink')))
+    return event.get('htmlLink')
 
   except HttpError as error:
     print(f"An error occurred: {error}")
-
-
-if __name__ == "__main__":
-  creds = auth()
-  create_event(creds, {})
